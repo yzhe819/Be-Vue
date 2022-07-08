8676b323396328f71137bc8674de167d8fa5c6e4
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-07-09 10:18:03 +1200

    add: reactive & effect

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,52 @@
+class ReactiveEffect {
+  private _fn: any;
+
+  constructor(fn, public scheduler?) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    return this._fn();
+  }
+}
+
+const targetMap = new Map();
+export function track(target, key) {
+  // target -> key -> dep
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
+  }
+}
+
+let activeEffect;
+export function effect(fn, options: any = {}) {
+  const scheduler = options.scheduler;
+  const _effect = new ReactiveEffect(fn, scheduler);
+
+  _effect.run();
+
+  return _effect.run.bind(_effect);
+}
---
+++ src/reactivity/test/effect.test.ts
@@ -0,0 +1,64 @@
+import { reactive } from "../reactive";
+import { effect } from "../effect";
+
+describe("effect", () => {
+  it("happy path", () => {
+    const user = reactive({ age: 10 });
+
+    let nextAge;
+    effect(() => {
+      nextAge = user.age + 1;
+    });
+    expect(nextAge).toBe(11);
+
+    // 更新数据
+    user.age++;
+    expect(nextAge).toBe(12);
+  });
+
+  it("should return runner when call effect", () => {
+    // effect 会返回一个function (就是runner)
+    // 调用runner可以返回内部的值
+    // effect -> function (runner) -> fn -> return value
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+
+  it("scheduler", () => {
+    // 通过effect的第二个参数给定一个scheduler的函数
+    // effect第一次执行的时候，还会执行fn，但是不会执行scheduler
+    // 当响应式对象 set update的时候，会执行scheduler，但不会执行fn
+    // 如果执行runner则会再次执行fn
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // should be called on first trigger
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    // // should not run yet
+    expect(dummy).toBe(1);
+    // // manually run
+    run();
+    // // should have run
+    expect(dummy).toBe(2);
+  });
+});
