0b5d142dafa04a961195c9a0cfe8adcd5ea97a30
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-07-09 10:37:25 +1200

    add: effect stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,8 @@
 class ReactiveEffect {
   private _fn: any;
-
+  deps = [];
+  active = true;
+  onStop?: () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -8,6 +10,21 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 
 const targetMap = new Map();
@@ -26,6 +43,7 @@ export function track(target, key) {
   }
 
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -45,8 +63,16 @@ let activeEffect;
 export function effect(fn, options: any = {}) {
   const scheduler = options.scheduler;
   const _effect = new ReactiveEffect(fn, scheduler);
+  _effect.onStop = options.onStop;
 
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
