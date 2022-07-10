0856b2f2957513d4e9a8c4d17497afb9c5e22686
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-07-11 07:49:10 +1200

    add: render element

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -4,12 +4,12 @@ export const App = {
   // 必须要写 render
   render() {
     // ui
-    return h("div", "hi, " + this.msg);
+    return h("div", "hi, be-vue");
   },
 
-  setup() {
-    return {
-      msg: "be-vue",
-    };
-  },
+  // setup() {
+  //   return {
+  //     msg: "be-vue",
+  //   };
+  // },
 };
--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,15 +1,54 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
-  // TODO 判断vnode 是不是一个 element
-  // 是 element 那么就应该处理 element
-  // 思考题： 如何去区分是 element 还是 component 类型呢？
-  // processElement();
   patch(vnode, container);
 }
 
 function patch(vnode, container) {
-  processComponent(vnode, container);
+  // TODO 判断vnode 是不是一个 element
+  // 是 element 那么就应该处理 element
+  // 思考题： 如何去区分是 element 还是 component 类型呢？
+  if (typeof vnode.type === "string") {
+    // 是element节点
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    // 是对象，作为组件处理
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  // 创建空的 dom 元素
+  const el = document.createElement(vnode.type);
+  const { children } = vnode;
+
+  // children
+  if (typeof children === "string") {
+    console.log("string children");
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  // props
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode: any, container: any) {
