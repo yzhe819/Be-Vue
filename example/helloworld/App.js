addf2bfea7f22bfdcd5c216286e90e71d0960939
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-07-11 23:50:28 +1200

    chore: update example

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -4,7 +4,15 @@ export const App = {
   // 必须要写 render
   render() {
     // ui
-    return h("div", {}, "hello world!");
+    return h(
+      "div", // div tag
+      {
+        id: "root", // set the id attribute
+        class: ["red", "hard"], // set the class attribute
+      },
+      // Array -> children -> two p tags
+      [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "be-vue")]
+    );
   },
 
   setup() {
